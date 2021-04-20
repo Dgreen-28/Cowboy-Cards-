77aa345492f6cdfd6af7ebee146f8a231eb15038
Decoeyon Green <king.dg28@gmail.com>
2021-04-20 15:47:56 -0500

    ...

---
+++ CreateSetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateSetViewController.swift
+//  Cowboy Cards!
+//
+//  Created by Decoreyon Green on 4/20/21.
+//
+
+import UIKit
+
+class CreateSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
