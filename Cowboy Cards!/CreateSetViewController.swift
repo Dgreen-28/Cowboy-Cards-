2492813136a386da62cd1679de6681f458f909c4
Decoeyon Green <king.dg28@gmail.com>
2021-04-20 17:00:46 -0500

    buttons working

--- Cowboy Cards!/CreateSetViewController.swift
+++ Cowboy Cards!/CreateSetViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class CreateSetViewController: UIViewController {
 
+    var questionArray = [String]()
+    var answerArray = [String]()
+
+    @IBOutlet weak var setTitleTextField: UITextField!
+    
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var answerTextView: UITextView!
+    
+    @IBAction func NextCardButtonTapped(_ sender: Any) {
+        questionArray.append("new")
+        answerArray.append("new")
+
+        print(questionArray)
+    }
+    
+    @IBAction func CreateSetButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    var questionArray = [String]()
-    var answerArray = [String]()
 
 }
